48d2be755df548543a1e0a312fc54c661541b95f
Ryan Yoon <sapphirus15@gmail.com>
2022-09-02 13:31:36 +0900

    Initial Commit

---
+++ JustWalking/JustWalkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustWalkingApp.swift
+//  JustWalking
+//
+//  Created by Ryan on 2022/09/02.
+//
+
+import SwiftUI
+
+@main
+struct JustWalkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
